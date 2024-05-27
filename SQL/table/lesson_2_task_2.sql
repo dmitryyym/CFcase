4d3834abb797f22516ce60844938abe4b7a33577
Dmitry Makarov <dmakarov0402@yandex.ru>
2024-05-27 11:11:58 +0300

    Урок 2 Задание 2. Создание таблицы студентов

--- SQL/table/lesson_2_task_2.sql
+++ SQL/table/lesson_2_task_2.sql
@@ -1,6 +1,7 @@
-CREATE TABLE students
-(
-    id INT PRIMARY KEY,
-    name varchar (30),
-    surname varchar (30)
-)
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'students')
+    CREATE TABLE students
+    (
+        id INT PRIMARY KEY,
+        name varchar (30),
+        surname varchar (30)
+    )
