4ca9e29a23242c93721e1572b599f2937007721f
Dmitry Makarov <dmakarov0402@yandex.ru>
2024-06-25 11:38:18 +0300

    Модуль 18 Задание 2

--- Module_18/task_2_part_1.sql
+++ Module_18/task_2_part_1.sql
@@ -1,26 +1,21 @@
 -- MS SQL
-CREATE FUNCTION dbo.func
-(
+CREATE PROCEDURE dbo.func
     @account_id INT,
     @session_id INT,
     @product_name VARCHAR(100)
-)
-RETURNS @table TABLE
-(
-    user_id INT,
-    product_type VARCHAR(100),
-    user_type VARCHAR(100),
-    session_id INT,
-    status INT
-) 
 AS
 BEGIN
-    INSERT INTO @table
     SELECT 
-        1 AS user_id,
-        'product_type' AS product_type,
-        'user_type' AS user_type,
+        t.user_id,
+        t.product_type,
+        t.user_type
+    FROM
+    (
+        VALUES (1, 'pr_type_1', 'usr_type_1'), (2, 'pr_type_2', 'usr_type_2')
+    ) AS t (user_id, product_type, user_type);
+    
+    SELECT 
         @session_id AS session_id,
         'success' AS status;
-    RETURN;
+    AS query_status (session_id, status);
 END;
--- Module_18/task_2_part_2.sql
+++ Module_18/task_2_part_2.sql
@@ -7,21 +7,41 @@ CREATE FUNCTION main.func
 ) 
 RETURNS TABLE
 (
-    user_id INT, 
-    product_type VARCHAR, 
-    user_type VARCHAR, 
-    session_id INT, 
-    status INT
-) 
+    first_query JSON
+    second_query JSON
+)
 LANGUAGE plpgsql 
 AS $body$
+DECLARE
+    first_select JSON;
+    second_select JSON;
 BEGIN
+    SELECT json_agg(t)
+    FROM
+    (
+        SELECT 
+            t.user_id,
+            t.product_type,
+            t.user_type
+        FROM
+        (
+            VALUES (1, 'pr_type_1', 'usr_type_1'), (2, 'pr_type_2', 'usr_type_2')
+        )
+        AS t (user_id, product_type, user_type)
+    ) AS t INTO first_select;
+
+    SELECT json_agg(s)
+    FROM
+    (
+        SELECT
+            in_session_id AS session_id,
+            'success' AS status
+        AS s (session_id, status)
+    ) AS query_status INTO second_select;
+
     RETURN QUERY
     SELECT 
-        1 AS user_id,
-        'prodcut_type' AS product_type,
-        'user_type' AS user_type,
-        in_session_id AS session_id,
-        'success' AS status;
+        first_select,
+        second_select;
 END;
 $body$;
